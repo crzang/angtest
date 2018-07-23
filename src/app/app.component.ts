fc01d1ef9ee21ae9b4a8ffed3468992d74802e44
va7entin <va7entin@gmail.com>
2018-07-23 11:13:21 +0700

    sign in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,26 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('titleEnter') titleEnter: ElementRef;
   @ViewChild('loginBlock') loginBlock: ElementRef;
   @ViewChild('passwordBlock') passwordBlock: ElementRef;
+  @ViewChild('btnEnter') btnEnter: ElementRef;
+  @ViewChild('lostPassword') lostPassword: ElementRef;
+  @ViewChild('helpToNewAccount') helpToNewAccount: ElementRef;
+  @ViewChild('signUp') signUp: ElementRef;
+  @ViewChild('helpdesk') helpdesk: ElementRef;
+  @ViewChild('footer') footer: ElementRef;
 
-  bgHeight = '0px';
+  baseHeight = 768;
   baseWith = 1024;
+  mainHeight = this.baseHeight+"px";
   logoOffset = '0px';
   constructor() {
-
-    this.bgHeight = window.innerHeight + 'px';
+    if (window.innerHeight > 768) {
+      this.baseHeight = window.innerHeight;
+    }
     if (window.innerWidth > 1024) {
       this.baseWith = window.innerWidth;
     }
     this.logoOffset = this.baseWith / 2 - 188 / 2 + 'px';
+    this.mainHeight=this.baseHeight+"px";
   }
 
 
@@ -29,6 +38,13 @@ export class AppComponent implements AfterViewInit {
     this.titleEnter.nativeElement.style.marginLeft = this.baseWith / 2 - this.titleEnter.nativeElement.offsetWidth / 2 + 'px';
     this.loginBlock.nativeElement.style.marginLeft = this.baseWith / 2 - this.loginBlock.nativeElement.offsetWidth / 2 + 'px';
     this.passwordBlock.nativeElement.style.marginLeft = this.baseWith / 2 - this.passwordBlock.nativeElement.offsetWidth / 2 + 'px';
+    this.btnEnter.nativeElement.style.marginLeft = this.baseWith / 2 - this.btnEnter.nativeElement.offsetWidth / 2 + 'px';
+    this.lostPassword.nativeElement.style.marginLeft = this.baseWith / 2 - this.lostPassword.nativeElement.offsetWidth / 2 + 'px';
+    this.footer.nativeElement.style.left = this.baseWith / 2 - this.footer.nativeElement.offsetWidth / 2 + 'px';
+    let lostPasswordPos=this.baseHeight- this.footer.nativeElement.offsetHeight - 50
+      //-this.lostPassword.nativeElement.offsetTop + this.lostPassword.nativeElement.offsetHeight;
+    //lostPasswordPos=lostPasswordPos
+    this.footer.nativeElement.style.top = lostPasswordPos + 'px';
   }
 
 }
