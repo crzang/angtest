d676765dc79eee72f400af5e0ee4e4054fec5d73
va7entin <va7entin@gmail.com>
2018-08-15 17:28:26 +0600

    styles fix

--- src/app/credit-info/credit-info.component.ts
+++ src/app/credit-info/credit-info.component.ts
@@ -13,23 +13,23 @@ export class CreditInfoComponent implements OnInit {
   constructor() {
 
     this.credits = [
-      {name: 'Начальная сумма кредита', value: '1000000 руб'},
-      {name: 'Финальная сумма кредита', value: '1058259 руб'},
+      {name: 'Начальная сумма кредита', value: '1000000 р'},
+      {name: 'Финальная сумма кредита', value: '1058259 р'},
       {name: 'Процентная ставка', value: '9,9%'},
       {name: 'Дата выдачи', value: '05.08.2017'},
       {name: 'Дата завершения', value: '05.09.2018'},
       {name: 'Срок кредита', value: '13 месяцев'},
-      {name: 'Ежемесячный платеж', value: '81438 руб'},
+      {name: 'Ежемесячный платеж', value: '81438 '},
       {name: 'Дата следующего платежа', value: '05.05.2018'},
 
     ];
     this.details = [
-      {date: '5 февраля', credit: '76 804 р', percent: '5 635 р', payment: '81 438', done: true, next: false},
-      {date: '5 февраля', credit: '76 804 р', percent: '5 635 р', payment: '81 438', done: false, next: true},
-      {date: '5 февраля', credit: '76 804 р', percent: '5 635 р', payment: '81 438', done: false, next: false},
-      {date: '5 февраля', credit: '76 804 р', percent: '5 635 р', payment: '81 438', done: false, next: false},
-      {date: '5 февраля', credit: '76 804 р', percent: '5 635 р', payment: '81 438', done: false, next: false},
-      {date: '5 февраля', credit: '76 804 р', percent: '5 635 р', payment: '81 438', done: false, next: false},
+      {date: '5 февраля', credit: '76 804 ', percent: '5 635 ', payment: '81 438', done: true, next: false},
+      {date: '5 февраля', credit: '76 804 ', percent: '5 635 ', payment: '81 438', done: false, next: true},
+      {date: '5 февраля', credit: '76 804 ', percent: '5 635 ', payment: '81 438', done: false, next: false},
+      {date: '5 февраля', credit: '76 804 ', percent: '5 635 ', payment: '81 438', done: false, next: false},
+      {date: '5 февраля', credit: '76 804 ', percent: '5 635 ', payment: '81 438', done: false, next: false},
+      {date: '5 февраля', credit: '76 804 ', percent: '5 635 ', payment: '81 438', done: false, next: false},
 
     ];
   }
